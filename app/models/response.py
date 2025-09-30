89482710556382b9e630e6032846f7fa9080eb4e
Kunashe-n <kunashendokanga04@gmail.com>
2025-09-30 15:38:31 +0200

    Delete app directory

--- app/models/response.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from datetime import datetime
-
-class Response(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    survey_id = db.Column(db.Integer, db.ForeignKey("survey.id"))
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    answers = db.Column(db.JSON)  # store answers in JSON format
-    submitted_at = db.Column(db.DateTime, default=datetime.utcnow)
